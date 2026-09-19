336e7c573237a7479c82ff4d23d0d4cd777d0dc1
Jeb Bisson <jebbisson@users.noreply.github.com>
2026-09-19 08:40:41 -0400

    fix: add Faker and initialize cart items

--- src/main/java/csd230/lab1/pojos/Cart.java
+++ src/main/java/csd230/lab1/pojos/Cart.java
@@ -1,9 +1,10 @@
 package csd230.lab1.pojos;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 public class Cart {
-    private List<Product> items;
+    private List<Product> items = new ArrayList<>();
 
     public Cart() {
     }
