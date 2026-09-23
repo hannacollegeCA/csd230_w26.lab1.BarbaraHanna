cb8a799c3e70ebd078260a67515319cf92a23b9b
BaHanna <bhsantos1424@gmail.com>
2026-09-23 13:40:27 -0400

    Updated ProductEntity (many-to-many), added CartEntity, and added isbn to BookEntity

--- src/main/java/csd230/lab1/entities/BookEntity.java
+++ src/main/java/csd230/lab1/entities/BookEntity.java
@@ -4,10 +4,33 @@ import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 @Entity @DiscriminatorValue("BOOK")
 public class BookEntity extends PublicationEntity {
+    private String isbn;
     private String author;
     public BookEntity() {}
-    public BookEntity(String t, double p, int c, String a) { super(t, p, c); this.author = a; }
-    public String getAuthor() { return author; }
-    public void setAuthor(String a) { this.author = a; }
-    @Override public String toString() { return "Book{author='" + author + "', " + super.toString() + "}"; }
+    public BookEntity(String t, double p, int c, String a) {
+        super(t, p, c);
+        this.author = a;
+        this.isbn = isbn;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String a) {
+        this.author = a;
+    }
+
+    public String getIsbn() {
+        return  isbn;
+    }
+
+    public void  setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{author='" + author + "', " + super.toString() + "}";
+    }
 }
